be02439e023d248f0149e21358a4cc1c254f20d2
deadseal001 <liwenbo4936@gmail.com>
2022-10-08 21:11:14 -0500

    promise used

--- src/page-template.js
+++ src/page-template.js
@@ -12,59 +12,53 @@ const generateAbout = aboutText =>{
 };
 
 
-    const generateProjects = projectsArr => {
-        return `
-          <section class="my-3" id="portfolio">
-            <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
-            <div class="flex-row justify-space-between">
-            ${projectsArr
-              .filter(({ feature }) => feature)
-              .map(({ name, description, languages, link }) => {
-                return `
-                <div class="col-12 mb-2 bg-dark text-light p-3">
-                  <h3 class="portfolio-item-title text-light">${name}</h3>
-                  <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(', ')}
-                  </h5>
-                  <p>${description}</p>
-                  <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-                </div>
-              `;
-              })
-              .join('')}
-      
-            ${projectsArr
-              .filter(({ feature }) => !feature)
-              .map(({ name, description, languages, link }) => {
-                return `
-                <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
-                  <h3 class="portfolio-item-title text-light">${name}</h3>
-                  <h5 class="portfolio-languages">
-                    Built With:
-                    ${languages.join(', ')}
-                  </h5>
-                  <p>${description}</p>
-                  <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
-                </div>
-              `;
-              })
-              .join('')}
+const generateProjects = projectsArr => {
+    return `
+        <section class="my-3" id="portfolio">
+        <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
+        <div class="flex-row justify-space-between">
+        ${projectsArr
+            .filter(({ feature }) => feature)
+            .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 mb-2 bg-dark text-light p-3">
+                <h3 class="portfolio-item-title text-light">${name}</h3>
+                <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(', ')}
+                </h5>
+                <p>${description}</p>
+                <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
             </div>
-          </section>
-        `;
-      };
+            `;
+            })
+            .join('')}
+    
+        ${projectsArr
+            .filter(({ feature }) => !feature)
+            .map(({ name, description, languages, link }) => {
+            return `
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+                <h3 class="portfolio-item-title text-light">${name}</h3>
+                <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(', ')}
+                </h5>
+                <p>${description}</p>
+                <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+            `;
+            })
+            .join('')}
+        </div>
+        </section>
+    `;
+    };
 
 
 module.exports=templatedData=> {
     console.log(templatedData);
-    // destructure projects and about data from templateData based on their property key names
-    // this will create three variables based on data in templateData
     const { projects, about, ...header } = templatedData;
-    // const header={
-    //     name: templatedData.name,
-    //     github: templatedData.github
-    // };
 
     return `
     <!DOCTYPE html>
@@ -81,7 +75,7 @@ module.exports=templatedData=> {
     <body>
         <header>
             <div class="container flex-row justify-space-bwtween align-center py-3">
-                <h1 class="page-title text-secondary bg-dark py-2 px-3>${header.name}</h1>
+                <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.name}</h1>
                 <nav class="flex-row">
                     <a class="ml-2 my-1 px-2 py-1 bg-secondary text-dark" href="https://github.com/${header.github}">Github</a>
                 </nav>
